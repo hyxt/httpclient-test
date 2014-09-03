7912417503508419e4b0e67bc8499acf5a376b5e
izerui <liuyuhua69@gmail.com>
2014-09-03 19:34:39 +0800

    修改接口

--- src/test/java/TestCase.java
+++ src/test/java/TestCase.java
@@ -8,28 +8,30 @@ import java.io.IOException;
  */
 public class TestCase {
 
+    public static final String token = "3xCfcAt4ssxkMEP4MOf/mGi83Ms=";
+
     //统一支付
     @Test
     public void test01() throws IOException {
-        SSLUtils.post("https://192.168.1.111:8443/rest/unifiedorder/?appToken=3xCfcAt4ssxkMEP4MOf/mGi83Ms=","unifiedorder.json");
+        SSLUtils.post("https://192.168.1.111:8443/rest/unifiedorder/?appToken="+token,"unifiedorder.json");
     }
 
     //订单列表查询
     @Test
     public void test02() throws IOException {
-        SSLUtils.post("https://192.168.1.111:8443/rest/orderQuery/?appToken=3xCfcAt4ssxkMEP4MOf/mGi83Ms=","orderQuery.json");
+        SSLUtils.post("https://192.168.1.111:8443/rest/orderQuery/?appToken="+token,"orderQuery.json");
     }
 
     //订单详情查询
     @Test
     public void test03() throws IOException {
-        SSLUtils.post("https://192.168.1.111:8443/rest/orderDetail/?appToken=3xCfcAt4ssxkMEP4MOf/mGi83Ms=","orderDetail.json");
+        SSLUtils.post("https://192.168.1.111:8443/rest/orderDetail/?appToken="+token,"orderDetail.json");
     }
 
     //退款
     @Test
     public void test04() throws IOException {
-        SSLUtils.post("https://192.168.1.111:8443/rest/refund/?appToken=3xCfcAt4ssxkMEP4MOf/mGi83Ms=","refund.json");
+        SSLUtils.post("https://192.168.1.111:8443/rest/refund/?appToken="+token,"refund.json");
     }
 
 }
